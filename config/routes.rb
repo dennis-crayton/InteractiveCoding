21c3490c9698cc2d6357dcdcd9265fca6ac78a09
Dennis Crayton <ethan.xyz@gmail.com>
2025-11-10 21:37:46 -0600

    navbar_updates
    updated navbar, concepts tab, and sandbox styling

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,16 @@ Rails.application.routes.draw do
   root "home#index"
 
   #Sandbox pages
-  get "/sandbox/python", to: "sandbox#python", as: :sandbox_python
-  get "/sandbox/ruby", to: "sandbox#ruby", as: :sandbox_ruby  
-  get "/sandbox/java", to: "sandbox#java", as: :sandbox_java
+  # get "/sandbox/python", to: "sandbox#python", as: :sandbox_python
+  # get "/sandbox/ruby", to: "sandbox#ruby", as: :sandbox_ruby  
+  # get "/sandbox/java", to: "sandbox#java", as: :sandbox_java
   
+  #Sandbox pages 
+  get "/languages/ruby/sandbox", to: "sandbox#ruby", as: :languages_ruby_sandbox
+  get "/languages/python/sandbox", to: "sandbox#python", as: :languages_python_sandbox
+  get "/languages/java/sandbox", to: "sandbox#java", as: :languages_java_sandbox
 
-  # Language pages
+  #Language pages
   get "/languages/ruby", to: "languages#ruby"
   get "/languages/python", to: "languages#python"
   get "/languages/java", to: "languages#java"
